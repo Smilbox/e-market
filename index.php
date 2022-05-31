1e6a02bb18e1b7b76181513aaec6f3cfb1b6ffee
Michael Randrianarisona <michael.randrianarisona@outlook.com>
2022-05-31 13:55:08 +0300

    Delpoyment mode

--- index.php
+++ index.php
@@ -67,9 +67,11 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
+		/*
 		ini_set('display_errors',0);
 		ini_set('display_startup_errors',0);
 		error_reporting(0);
+		*/
 	break;
 
 	case 'testing':
